ebb910f752ab6b0c72419dfd12d56bd2096e5cbc
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-02-24 08:48:48 -0800

    key
    different key used

--- script.js
+++ script.js
@@ -7,16 +7,18 @@ let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 30;
-const apiKey = '47Q7LUlv6un0a3KdtSxdLlcJE_xqu_LTV9HKwNeLMAY';
-const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
-
-// check if all images were loaded
+let count = 5;
+const apiKey = 'jFgS8tteGD425f4oZfygQVaVnD6gt6GucN2yyz3xFek';
+let apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+ 
+// Check if all images were loaded
 function imageLoaded() {
   imagesLoaded++;
   if (imagesLoaded === totalImages) {
     ready = true;
     loader.hidden = true;
+    count = 30
+    apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
   }
 }
 
